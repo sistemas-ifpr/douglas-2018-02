49e247a23e7ba42e788346e44fd34843cde37fe3
DouglasBergamaschi <bergamaschidouglas@gmail.com>
2018-08-04 21:06:16 -0300

    1º Entrega
    Alteração Pagina Inicial

---
+++ views/site/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Biblioteca Municipal';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Biblioteca Municipal De Abelardo Luz</h1>
+
+        <p class="lead">A biblioteca é um universo de livros.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Venha Conferir Nossa Coleção!</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>O Teatro Abre Caminhos</h2>
+
+                <p>Um perfil do múltiplo Ivam Cabral, cofundador da companhia Os Satyros, que neste ano ultrapassou a marca de 100 espetáculos produzidos</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>REGISTRO DE OBRAS INTELECTUAIS - DICAS IMPORTANTES</h2>
+
+                <p>Com base nas dúvidas mais frequentes em relação ao Escritório de Direitos Autorais, a Fundação Biblioteca Nacional elaborou um pequeno guia para orientar os usuários durante o processo de registro. Confira:</p>
+
+                <p><a class="btn btn-default" href="https://www.bn.gov.br/acontece/noticias/2016/10/registro-obras-intelectuais-eda-escritorio-direitos">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
