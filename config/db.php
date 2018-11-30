19c631297f2ec4c095aab66a4637d578c5974ca1
DouglasBergamaschi <bergamaschidouglas@gmail.com>
2018-11-30 21:11:06 -0200

    Update config/db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=biblioteca',
     'username' => 'root',
-    'password' => '',
+    'password' => '',//senha biblioteca@ para o 000webhost
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
